520561c8c9d5d760d23bc5913666308cd3a84661
9te <wangzihao139@126.com>
2024-10-06 21:42:57 +0800

    feat: describe

--- Innovachain_frontend/src/constants/index.ts
+++ Innovachain_frontend/src/constants/index.ts
@@ -1,2 +1,3 @@
 export const WAGMI_STORAGE_KEY = "INNOVA_CHAIN_WAGMI_STORAGE";
 export const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+export const MIDJOURNEY_API_URL = import.meta.env.VITE_MIDJOURNEY_API_URL || "http://127.0.0.1:8080";
--- Innovachain_frontend/src/pages/ProductDetail/OriginalStage.tsx
+++ Innovachain_frontend/src/pages/ProductDetail/OriginalStage.tsx
@@ -3,71 +3,55 @@ import { HeartImg, UserImg } from "../../assets/gallery";
 import { Avatar2Img, AvatarImg } from "../../assets/product-detail";
 import { CardContainer } from "../../components/Card";
 import { twc } from "react-twc";
+import { ProductInfo } from "../../utils/api";
 
-const OriginalStage = ({
-  onClickRecreated,
-}: {
-  onClickRecreated?: () => void;
-}) => {
-  return (
-    <CardContainer className="flex-1">
-      {/* Choice Card */}
-      <div className="flex justify-between items-end">
-        <h3 className="text-[#292B39] text-[50px] font-semibold font-poppins leading-[35px]">
-          Azuki
-        </h3>
-        <div className="flex gap-1">
-          <button className={clsx(
-            "bg-[#0066D4] px-2 py-1 rounded-full text-white font-medium"
-          )}>
-            Verified
-          </button>
-        </div>
-      </div>
-      <p className="text-[#888888B2] text-xl mt-10 mb-20">Recreation of famous NFT Azuki.</p>
-      <div className="flex gap-2">
-        <img className="w-[65px] h-[65px] rounded-full" src={Avatar2Img} />
-        <div>
-          <p className="text-lg text-[#8d8d8d]">Former creator</p>
-          <p className="text-2xl font-semibold">Perperzon</p>
-        </div>
-      </div>
-      <div className="flex gap-2 mt-4">
-        <img className="w-[65px] h-[65px] rounded-full" src={AvatarImg} />
-        <div>
-          <p className="text-lg text-[#8d8d8d]">Current creator</p>
-          <p className="text-2xl font-semibold">Videz</p>
-        </div>
-      </div>
-      <div className="flex justify-end gap-4 my-6">
-        <p className="text-[#8E8E8E] text-xl font-medium leading-[20px]">
-          20
-        </p>
-        <img className="w-[20px] h-[20px]" src={UserImg} />
-        <p className="text-[#8E8E8E] text-xl font-medium leading-[20px]">
-          20
-        </p>
-        <img className="w-[20px] h-[20px]" src={HeartImg} />
-      </div>
-      <div className="flex gap-2 justify-end">
-        <p className="text-[#8c8c8c]">Created at</p>
-        <p className="text-black">Jun 17, 2023 at 05:08</p>
-      </div>
-      <div className="mt-5 flex flex-col gap-2">
-        <DarkButton>
-          Purchase
-        </DarkButton>
-        <DarkButton className="w-full h-20 rounded-[20px] bg-black text-white font-bold  text-lg"
-          onClick={onClickRecreated}>
-          Recreated
-        </DarkButton>
-      </div>
-    </CardContainer>
-  );
+const OriginalStage = ({ onClickRecreated, info }: { onClickRecreated?: () => void; info: ProductInfo }) => {
+    return (
+        <CardContainer className="flex-1">
+            {/* Choice Card */}
+            <div className="flex justify-between items-end">
+                <h3 className="text-[#292B39] text-[50px] font-semibold font-poppins leading-[35px]">{info.name}</h3>
+                <div className="flex gap-1">
+                    <button className={clsx("bg-[#0066D4] px-2 py-1 rounded-full text-white font-medium")}>Verified</button>
+                </div>
+            </div>
+            <p className="text-[#888888B2] text-xl mt-10 mb-20">{info.description}</p>
+            <div className="flex gap-2">
+                <img className="w-[65px] h-[65px] rounded-full" src={Avatar2Img} />
+                <div>
+                    <p className="text-lg text-[#8d8d8d]">Former creator</p>
+                    <p className="text-2xl font-semibold">Perperzon</p>
+                </div>
+            </div>
+            <div className="flex gap-2 mt-4">
+                <img className="w-[65px] h-[65px] rounded-full" src={AvatarImg} />
+                <div>
+                    <p className="text-lg text-[#8d8d8d]">Current creator</p>
+                    <p className="text-2xl font-semibold">Videz</p>
+                </div>
+            </div>
+            <div className="flex justify-end gap-4 my-6">
+                <p className="text-[#8E8E8E] text-xl font-medium leading-[20px]">20</p>
+                <img className="w-[20px] h-[20px]" src={UserImg} />
+                <p className="text-[#8E8E8E] text-xl font-medium leading-[20px]">20</p>
+                <img className="w-[20px] h-[20px]" src={HeartImg} />
+            </div>
+            <div className="flex gap-2 justify-end">
+                <p className="text-[#8c8c8c]">Created at</p>
+                <p className="text-black">Jun 17, 2023 at 05:08</p>
+            </div>
+            <div className="mt-5 flex flex-col gap-2">
+                <DarkButton>Purchase</DarkButton>
+                <DarkButton className="w-full h-20 rounded-[20px] bg-black text-white font-bold  text-lg" onClick={onClickRecreated}>
+                    Recreated
+                </DarkButton>
+            </div>
+        </CardContainer>
+    );
 };
 const DarkButton = twc.div`
   w-full h-20 rounded-[20px] bg-black text-white font-bold  text-lg
-  flex justify-center items-center 
+  flex justify-center items-center hover:cursor-pointer 
 `;
 
 export default OriginalStage;
