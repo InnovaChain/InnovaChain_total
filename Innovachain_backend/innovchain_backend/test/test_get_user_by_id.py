b5801a6b243c55c765ed6f948afd8b9e47f1d3dd
0xyoung <35057221+0xy0ung@users.noreply.github.com>
2024-10-06 22:55:25 +0800

    feat: add get user info by user id endpoint (#13)

---
+++ Innovachain_backend/innovchain_backend/test/test_get_user_by_id.py
@@ -0,0 +1,15 @@
+import requests
+
+def test_read_image(user_id):
+    url = f"http://localhost:8000/users/{user_id}"
+    response = requests.get(url)
+
+    print(f"Response status code: {response.status_code}")
+
+    if response.status_code == 200:
+        print(f"Response JSON: {response.json()}")
+    else:
+        print(f"Error message: {response.text}")
+
+if __name__ == "__main__":
+    test_read_image(1)
