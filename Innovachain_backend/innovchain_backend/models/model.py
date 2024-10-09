1663c44c4fed20da4ecba1e11d3772519fee506b
0xyoung <35057221+0xy0ung@users.noreply.github.com>
2024-10-10 00:02:44 +0800

    fix: add alembic script for foreign key (#29)

--- Innovachain_backend/innovchain_backend/models/model.py
+++ Innovachain_backend/innovchain_backend/models/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String, DateTime, func
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime
@@ -9,7 +9,7 @@ class Image(Base):
     __tablename__ = "images"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey('users.id')) 
     user = relationship("User", back_populates="images")
     filename = Column(String, index=True)
     image_path = Column(String)
@@ -26,3 +26,4 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     wallet_address = Column(String, index=True)
+    images = relationship("Image", back_populates="user")
