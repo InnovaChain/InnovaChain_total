cd1ce0f38a06e7bbbbdb640246c53a123db196e9
y3-web3 <35057221+y3-web3@users.noreply.github.com>
2024-11-28 02:07:41 +0800

    feat: increment image reference count

--- Innovachain_backend/innovchain_backend/models/images_repository.py
+++ Innovachain_backend/innovchain_backend/models/images_repository.py
@@ -187,6 +187,29 @@ class ImageRepository:
                 raise e
         return db_image
 
+    async def increment_reference_count_by(self, image_id: int, count: int):
+        db_image = self.db.query(Image).filter(Image.id == image_id).first()
+        if db_image:
+            if db_image.reference_count == 0:
+                reference_count = (
+                    self.db.query(func.count(Image.id))
+                    .filter(Image.source_image_id == db_image.id)
+                    .scalar() or 0
+                )
+                db_image.reference_count = reference_count
+
+            db_image.reference_count += count
+            
+            try:
+                await self.user_stats_repo.update_user_stats_references(db_image.user_id, count)
+                self.db.commit()
+                self.db.refresh(db_image)
+            except SQLAlchemyError as e:
+                self.db.rollback()
+                raise e
+        return db_image
+
+
     async def update_image_reward(self, image_id: int, reward: float):
         db_image = self.db.query(Image).filter(Image.id == image_id).one_or_none()
         if db_image:
--- Innovachain_backend/innovchain_backend/test/test_increment_reference.py
+++ Innovachain_backend/innovchain_backend/test/test_increment_reference.py
@@ -1,8 +1,14 @@
 import requests
 
-def test_increment_reference_count(image_id):
+def test_increment_reference_count(image_id, count):
     url = f"http://localhost:8000/images/{image_id}/reference/increment"
-    response = requests.post(url)
+    
+    params = {}
+
+    if count is not None:
+        params['count'] = count
+
+    response = requests.post(url, params=params)
 
     data = response.json()
     print(f"Response status code: {response.status_code}")
@@ -10,4 +16,4 @@ def test_increment_reference_count(image_id):
         print(f"Updated reference count for image {image_id} is {data['reference_count']}")
 
 if __name__ == "__main__":
-    test_increment_reference_count(2)
+    test_increment_reference_count(2, 2)
