af8baa6a8109773a574e5790ddd0c34ae8820127
9te <wangzihao139@126.com>
2024-12-15 02:02:24 +0800

    Update Web3Provider.tsx

--- Innovachain_frontend/src/context/Web3Provider.tsx
+++ Innovachain_frontend/src/context/Web3Provider.tsx
@@ -18,12 +18,7 @@ const config = createConfig(
         walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
 
         // Required App Info
-        appName: "Your App Name",
-
-        // Optional App Info
-        appDescription: "Your App Description",
-        appUrl: "https://family.co", // your app's url
-        appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+        appName: "ArtCycle",
     })
 );
 
